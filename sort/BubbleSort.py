cb21de0e78f16856d8bfb89423554884a78a8156
eg <eungoo.jung@gameone.kr>
2016-11-21 15:07:07 +0900

    init

--- sort/BubbleSort.py
+++ sort/BubbleSort.py
@@ -1,14 +1,14 @@
 import unittest
 
-def bubblesort(alist):
-    for i in range(len(alist)-1):
-        for j in range(len(alist)-1):
-            if alist[j] > alist[j+1]:
-                alist[j], alist[j+1] = alist[j+1], alist[j]
-    return alist
+def bubbleSort(targetList):
+    for i in range(len(targetList)-1):
+        for j in range(len(targetList)-1):
+            if targetList[j] > targetList[j+1]:
+                targetList[j], targetList[j+1] = targetList[j+1], targetList[j]
+    return targetList
 
 class unit_test(unittest.TestCase):
     def test(self):
-        self.assertEqual([1, 2, 3, 4, 5, 6], bubblesort([4, 6, 1, 3, 5, 2]))
-        self.assertEqual([1, 2, 3, 4, 5, 6], bubblesort([6, 4, 3, 1, 2, 5]))
-        self.assertEqual([1, 2, 3, 4, 5, 6], bubblesort([6, 5, 4, 3, 2, 1]))
+        self.assertEqual([1, 2, 3, 4, 5, 6], bubbleSort([4, 6, 1, 3, 5, 2]))
+        self.assertEqual([1, 2, 3, 4, 5, 6], bubbleSort([6, 4, 3, 1, 2, 5]))
+        self.assertEqual([1, 2, 3, 4, 5, 6], bubbleSort([6, 5, 4, 3, 2, 1]))
