8a8cb8c871235039797e1d4319d381f7d58bf2e4
eg <eungoo.jung@gameone.kr>
2016-11-29 08:54:31 +0900

    Python Basic Tutorial - List

--- basic/List.py
+++ basic/List.py
@@ -35,12 +35,10 @@ print(a)
 del a[1]
 print(a)
 
-a = [1, 2, 3]
-a.append(4)
+a = [1, 2, 3, 4, [5, 6]]
 print(a)
-a.append([5,6])
 print(a)
-a = [1,2,5,6,3,4]
+a = [1, 2, 5, 6, 3, 4]
 a.sort()
 print(a)
 a.reverse()
@@ -57,4 +55,3 @@ print(a)
 print(a.count(6))
 a.extend([1, 2, 3, 4])
 print(a)
-
